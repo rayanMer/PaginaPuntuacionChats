5ad2c1a5c22ffd475e35737488f5773cdc5691f4
rayanMer <rayanoujda708@gmail.com>
2025-05-19 11:42:44 +0200

    Modificar componente valoraciones

---
+++ src/components/PaginaValoraciones.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PaginaValoraciones = () => {
+  return (
+    <div className="historial-container">
+      <h1>Vite + React + Tailwind CSS</h1>
+
+    </div>
+  );
+};
+
+export default PaginaValoraciones;
