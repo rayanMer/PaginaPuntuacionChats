3ad2d112e94964c53703151b1fdc858d0f22786b
JPalos21 <javier26paez@gmail.com>
2025-05-19 13:19:26 +0200

    sin rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import { AuthProvider } from './services/login/AuthProvider'
-import LoginPage from "./components/Login"
-
+import { AuthProvider } from "./services/login/AuthProvider";
+import LoginPage from "./components/Login";
+import { Routes, Route } from "react-router";
+import RutasProtegidas from "./services/login/RutasProtegidas";
+import PaginaValoraciones from "./components/PaginaValoraciones";
+import Historial from "./components/Historial";
 
 function App() {
   return (
     <AuthProvider>
       <div>
         <Routes>
+          <Route path="/login" element={<LoginPage />} />
+
           <Route
-            path="/login"
-            element={<LoginPage/>}
+            path="/"
+            element={
+            
+                <PaginaValoraciones />
+              
+            }
+          />
+
+          <Route
+            path="/historial"
+            element={
+            
+                <Historial />
+              
+            }
           />
         </Routes>
       </div>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
