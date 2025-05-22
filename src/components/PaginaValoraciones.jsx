4be35bae5a4d8ec3b93f3f38006259003ee3d1a7
Luis Anzules <abluis@Luis-PC>
2025-05-22 12:47:27 +0200

    Añadido alerta generica en sweetAlert

--- src/components/PaginaValoraciones.jsx
+++ src/components/PaginaValoraciones.jsx
@@ -4,6 +4,7 @@ import ServicioLecturaConversacion from '../services/ServicioLecturaConversacion
 import PanelConversacion from './PaginaValoraciones/PanelConversacion';
 import PanelMetricas from './PaginaValoraciones/PanelMetricas';
 import { useAuth } from '../services/login/AuthProvider';
+
 export default function PaginaValoraciones() {
   const [conversaciones, setConversaciones] = useState([]);
   const [conversacionesNoValoradas, setConversacionesNoValoradas] = useState([]);
