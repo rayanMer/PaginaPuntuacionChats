3b95b14b167576eb062352ddb154c1eaf940bc75
Luis Anzules <abluis@Luis-PC.localdomain>
2025-05-30 12:07:10 +0200

    Añadida la ruta para obtener una conversation por id

--- api-conversaciones-prueba/src/routes/conversations.py
+++ api-conversaciones-prueba/src/routes/conversations.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from typing import List
 from src.database import conversation_collection
 from src.models import Conversation
@@ -13,3 +13,13 @@ async def get_all_conversations():
         conversations.append(conversation)
     return conversations
 
+@router.get("/conversations/{conversation_id}", response_model=Conversation)
+async def get_conversation_by_id(conversation_id: str):
+    conversation = await conversation_collection.find_one({"id": conversation_id})
+    if not conversation:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Conversation with the id {conversation_id} was not found"
+        )
+    conversation["_id"] = str(conversation["_id"])
+    return conversation
